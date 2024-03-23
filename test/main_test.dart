3c710de351e26ef7b2924837f83fff2ca3e83c49
pablospizzamiglio <spizzamiglio.pablo@gmail.com>
2024-03-23 03:56:46 -0300

    Prueba widget principal

---
+++ test/main_test.dart
@@ -0,0 +1,10 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mascotas/main.dart';
+
+void main() {
+  testWidgets("Hello Widget", (WidgetTester tester) async {
+    await tester.pumpWidget(const MainApp());
+    await tester.pump();
+    expect(find.text('Hello World!'), findsOneWidget);
+  });
+}
