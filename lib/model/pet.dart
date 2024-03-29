a1534c923c594b8b78ae2d28141b95ecde084461
pablospizzamiglio <spizzamiglio.pablo@gmail.com>
2024-03-29 15:35:44 -0300

    Interpreta correctamente una fecha

--- lib/model/pet.dart
+++ lib/model/pet.dart
@@ -1,15 +1,17 @@
-class Pet {
-  int id;
-  String name;
-  int age;
-  double weight;
-  String photo;
-  DateTime birthdate;
-  String breed;
-  String fur;
-  String sex;
+import 'package:intl/intl.dart';
 
-  Pet({
+class Pet {
+  final int id;
+  final String name;
+  final int age;
+  final double weight;
+  final String photo;
+  final DateTime birthdate;
+  final String breed;
+  final String fur;
+  final String sex;
+
+  const Pet({
     required this.id,
     required this.name,
     required this.age,
@@ -20,4 +22,15 @@ class Pet {
     required this.fur,
     required this.sex,
   });
+
+  Pet.fromJson(Map<String, dynamic> json)
+      : id = json["id"] as int,
+        name = json["name"] as String,
+        age = json["age"] as int,
+        weight = json["weight"] as double,
+        photo = json["photo"] as String,
+        birthdate = DateFormat("yyyy-mm-dd").parse(json["birthdate"]),
+        breed = json["breed"] as String,
+        fur = json["fur"] as String,
+        sex = json["sex"] as String;
 }
