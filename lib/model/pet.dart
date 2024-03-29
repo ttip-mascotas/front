898738bcaadf457aca35b89c841cad29ac2e5c82
pablospizzamiglio <spizzamiglio.pablo@gmail.com>
2024-03-29 02:46:30 -0300

    Agrega modelo de Pet

---
+++ lib/model/pet.dart
@@ -0,0 +1,23 @@
+class Pet {
+  int id;
+  String name;
+  int age;
+  double weight;
+  String photo;
+  DateTime birthdate;
+  String breed;
+  String fur;
+  String sex;
+
+  Pet({
+    required this.id,
+    required this.name,
+    required this.age,
+    required this.weight,
+    required this.photo,
+    required this.birthdate,
+    required this.breed,
+    required this.fur,
+    required this.sex,
+  });
+}
