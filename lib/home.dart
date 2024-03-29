0c70a7c72cc5b8a429a449c04946f8073aa33366
pablospizzamiglio <spizzamiglio.pablo@gmail.com>
2024-03-29 15:46:24 -0300

    Implementa la carga de datos de una mascota

--- lib/home.dart
+++ lib/home.dart
@@ -1,9 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:mascotas/datasource/api.dart';
+import 'package:mascotas/datasource/pets_datasource.dart';
 import 'package:mascotas/model/pet.dart';
 import 'package:mascotas/pet_detail.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  final petDatasource = PetsDatasource(api: Api());
 
   @override
   Widget build(BuildContext context) {
@@ -16,21 +25,29 @@ class Home extends StatelessWidget {
         centerTitle: true,
         backgroundColor: Colors.purple.shade200,
       ),
-      body: PetDetail(pet: getPet()),
+      body: makePetDetails(),
     );
   }
-}
 
-Pet getPet() {
-  return Pet(
-    id: 1,
-    name: "Pippin",
-    age: 6,
-    weight: 8.2,
-    photo: "",
-    birthdate: DateTime.utc(2004, 11, 20),
-    breed: "Callejero",
-    fur: "Largo",
-    sex: "MALE",
-  );
+  FutureBuilder<Pet> makePetDetails() {
+    return FutureBuilder<Pet>(
+      future: petDatasource.getPet(1),
+      builder: (BuildContext context, AsyncSnapshot<Pet> snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(child: CircularProgressIndicator());
+        } else if (snapshot.hasError) {
+          return Container(
+            padding: const EdgeInsets.all(20),
+            child: Row(
+              children: [
+                Flexible(child: Text("${snapshot.error}")),
+              ],
+            ),
+          );
+        } else {
+          return PetDetail(pet: snapshot.data!);
+        }
+      },
+    );
+  }
 }
