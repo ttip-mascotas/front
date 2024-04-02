dcc69fa2496045f1e05df317530869cde6886ef3
ximena-bogado <ximenakbq@gmail.com>
2024-04-02 05:28:48 -0300

    fix overflow modal

--- lib/widget/medical_visit_registration_form.dart
+++ lib/widget/medical_visit_registration_form.dart
@@ -27,54 +27,56 @@ class _MedicalVisitRegistrationFormState
     return Padding(
       padding:
           EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom),
-      child: Form(
-        key: _formKey,
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 16.0),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              const InputTitle(
-                title: "Especialista",
-              ),
-              TextFormField(
-                controller: _specialistController,
-                validator: emptyFieldValidator,
-              ),
-              const InputTitle(
-                title: "Dirección",
-              ),
-              TextFormField(
-                controller: _addressController,
-                validator: emptyFieldValidator,
-              ),
-              const InputTitle(
-                title: "Fecha",
-              ),
-              TextFormField(
-                controller: _dateController,
-                validator: emptyFieldValidator,
-                readOnly: true,
-                onTap: selectDate,
-              ),
-              const InputTitle(
-                title: "Observaciones",
-              ),
-              TextFormField(
-                controller: _observationsController,
-                maxLines: 2,
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 8.0),
-                child: ElevatedButton(
-                    onPressed: saveMedicalVisit,
-                    child: const Text(
-                      "Registrar",
-                      style: TextStyle(color: Colors.white),
-                    )),
-              )
-            ],
+      child: SingleChildScrollView(
+        child: Form(
+          key: _formKey,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                const InputTitle(
+                  title: "Especialista",
+                ),
+                TextFormField(
+                  controller: _specialistController,
+                  validator: emptyFieldValidator,
+                ),
+                const InputTitle(
+                  title: "Dirección",
+                ),
+                TextFormField(
+                  controller: _addressController,
+                  validator: emptyFieldValidator,
+                ),
+                const InputTitle(
+                  title: "Fecha",
+                ),
+                TextFormField(
+                  controller: _dateController,
+                  validator: emptyFieldValidator,
+                  readOnly: true,
+                  onTap: selectDate,
+                ),
+                const InputTitle(
+                  title: "Observaciones",
+                ),
+                TextFormField(
+                  controller: _observationsController,
+                  maxLines: 2,
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 8.0),
+                  child: ElevatedButton(
+                      onPressed: saveMedicalVisit,
+                      child: const Text(
+                        "Registrar",
+                        style: TextStyle(color: Colors.white),
+                      )),
+                )
+              ],
+            ),
           ),
         ),
       ),
