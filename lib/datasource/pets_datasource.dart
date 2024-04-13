e21f644eed6bf2596cff7f2e4bb7b47e9c2e7ac7
pablospizzamiglio <spizzamiglio.pablo@gmail.com>
2024-04-13 00:53:38 -0300

    Implementa método para registrar una mascota

--- lib/datasource/pets_datasource.dart
+++ lib/datasource/pets_datasource.dart
@@ -1,7 +1,6 @@
 import 'dart:async';
 import 'dart:convert';
 
-import 'package:flutter/cupertino.dart';
 import 'package:http/http.dart';
 import 'package:mascotas/datasource/api.dart';
 import 'package:mascotas/exception/datasource_exception.dart';
@@ -33,20 +32,36 @@ class PetsDatasource {
 
     return _manageResponse<List<Pet>>(
       response,
-      parseJson: (json) => json['results'].map<Pet>((json) => Pet.fromJson(json)).toList(),
+      parseJson: (json) =>
+          json['results'].map<Pet>((json) => Pet.fromJson(json)).toList(),
       message: "Failed to load pets",
     );
   }
 
-  Future<MedicalVisit> addMedicalVisit(MedicalVisit medicalVisit, int id) async {
-    final body = medicalVisit.toJson();
+  Future<Pet> addPet(Pet pet) async {
+    try {
+      final body = pet.toJson();
+      final response = await api.post("/pets", body: body);
 
+      return _manageResponse<Pet>(
+        response,
+        parseJson: (json) => Pet.fromJson(json),
+        message: "Hubo un problema al registrar la mascota",
+      );
+    } on TimeoutException {
+      return throw DatasourceException(
+          "Nuestros servidores están ocupados, intentalo nuevamente más tarde.");
+    }
+  }
+
+  Future<MedicalVisit> addMedicalVisit(
+      MedicalVisit medicalVisit, int id) async {
+    final body = medicalVisit.toJson();
     final response = await api.post("/pets/$id/medical-records", body: body);
 
-    return _manageResponse<MedicalVisit>(
-        response,
+    return _manageResponse<MedicalVisit>(response,
         parseJson: (json) => MedicalVisit.fromJson(json),
-        message: "Hubo un problema al registrar la visita medica");
+        message: "Hubo un problema al registrar la visita médica");
   }
 
   T _manageResponse<T>(Response response,
@@ -54,10 +69,12 @@ class PetsDatasource {
     final Map<String, dynamic> json = jsonDecode(response.body);
     if (_isSuccessful(response)) {
       return parseJson(json);
-    } if (_isClientError(response)){
-      throw DatasourceException( json['message'], );
+    }
+    if (_isClientError(response)) {
+      throw DatasourceException(
+        json['message'],
+      );
     } else {
-      debugPrint(json['message']);
       throw DatasourceException(message);
     }
   }
@@ -66,5 +83,5 @@ class PetsDatasource {
       response.statusCode >= 200 || response.statusCode < 300;
 
   bool _isClientError(Response response) =>
-    response.statusCode >= 400 || response.statusCode < 500;
+      response.statusCode >= 400 || response.statusCode < 500;
 }
