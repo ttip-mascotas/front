5111a793b5eb7441291c43b7b877be5360e33dee
pablospizzamiglio <spizzamiglio.pablo@gmail.com>
2024-05-04 01:18:41 -0300

    Agrega listado de análisis a la vista de la pestaña

--- lib/widget/analysis_tab_view.dart
+++ lib/widget/analysis_tab_view.dart
@@ -1,15 +1,75 @@
 import 'package:flutter/material.dart';
+import 'package:mascotas/model/analysis.dart';
+import 'package:mascotas/utils/format.dart';
+import 'package:mascotas/widget/pets_divider.dart';
 
 class AnalysisTabView extends StatelessWidget {
+  final List<Analysis> analyses;
+
   const AnalysisTabView({
     super.key,
+    required this.analyses,
   });
 
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.all(16.0),
-      child: Text('No hay analisis registrados'),
+    return analyses.isNotEmpty
+        ? ListView.separated(
+            itemBuilder: (context, index) {
+              final analysis = analyses[index];
+              return Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    AnalysisDetail(
+                      text: analysis.name,
+                      icon: Icons.picture_as_pdf_rounded,
+                    ),
+                    AnalysisDetail(
+                      text: formatBytes(analysis.size, 1),
+                      icon: Icons.download_for_offline_rounded,
+                    ),
+                    AnalysisDetail(
+                      text: formatDateToString(analysis.createdAt),
+                      icon: Icons.calendar_month_rounded,
+                    )
+                  ],
+                ),
+              );
+            },
+            separatorBuilder: (context, index) => const PetsDivider(),
+            itemCount: analyses.length)
+        : const Padding(
+            padding: EdgeInsets.all(16.0),
+            child: Text('No hay analisis registrados'),
+          );
+  }
+}
+
+class AnalysisDetail extends StatelessWidget {
+  final IconData icon;
+  final String text;
+
+  const AnalysisDetail({
+    super.key,
+    required this.text,
+    required this.icon,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 8.0),
+      child: Row(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(right: 8.0),
+            child: Icon(icon, color: Colors.purple.shade200),
+          ),
+          Text(text),
+        ],
+      ),
     );
   }
 }
